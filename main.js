1092b0d80a7999444cd14368675a60e39294298b
Namdoboy <132242807+Namdoboy@users.noreply.github.com>
2023-10-28 11:02:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ async function main() {
     // 무작위 이미지 생성 및 애니메이션 함수
     function createRandomImage() {
         var imageElement = document.createElement('png');
-        imageElement.src = '/Users/jangminseog/Desktop/untitled folder 24/untitled folder/장민석_로고777 copy.png'; // 이미지 파일의 경로를 입력하세요
+        imageElement.src = ''; // 이미지 파일의 경로를 입력하세요
         imageElement.className = 'floating-image';
         var randomSize = getRandomSize();
         imageElement.style.width = randomSize + 'px';
